1c4abdbf767a9027ad2f321c412d693491857a0e
git <claytnmorais@gmail.com>
2024-03-04 23:28:16 -0300

    add notification project

---
+++ service--notification/src/main/java/com/shoesclick/service/notification/ServiceNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.shoesclick.service.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceNotificationApplication.class, args);
+	}
+
+}
---
+++ service--notification/src/test/java/com/shoesclick/service/notification/ServiceNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoesclick.service.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
